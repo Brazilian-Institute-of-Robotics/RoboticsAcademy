9f609447dffa437ebbec768dcaace74de8d42abf
barretto-dev <barretto.dev@gmail.com>
2025-05-06 08:13:31 -0300

    change function containers_exists only to check 2 containers

--- scripts/develop_academy.sh
+++ scripts/develop_academy.sh
@@ -33,9 +33,9 @@ cleanup() {
 }
 
 # Function that verify if the containers "developer-webapp" 
-# "developer-manager"  and "universe_db" exists
+# and "universe_db" exists
 containers_exist() {
-  docker ps -a --format '{{.Names}}' | grep -E 'developer-webapp|developer-manager|universe_db' > /dev/null
+  docker ps -a --format '{{.Names}}' | grep -E 'developer-webapp|universe_db' > /dev/null
 }
 
 while getopts ":r:b:i:g:n:t:h" opt; do
@@ -143,7 +143,7 @@ if [ "$nvidia" = "true" ]; then
 fi
 cp compose_cfg/$compose_file.yaml docker-compose.yaml
 
-# Containers "developer-manager", "developer-webapp" and "universe_db" exists
+# Containers "developer-webapp" and "universe_db" exists
 # and just need to restart
 if containers_exist; then
   docker compose start
