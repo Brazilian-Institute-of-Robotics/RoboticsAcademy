6694446db845329d30df0db6edeba10804cc5922
barretto-dev <barretto.dev@gmail.com>
2025-05-12 13:34:13 -0300

    Change where code files request is made
    Now the component ExerciseControl is responsible to request API the files codes of user

--- react_frontend/src/components/buttons/LoadServerFile.js
+++ react_frontend/src/components/buttons/LoadServerFile.js
@@ -2,51 +2,12 @@ import CloudUploadOutlinedIcon from "@mui/icons-material/CloudUploadOutlined";
 import { Box, Button } from "@mui/material";
 import * as React from "react";
 import PropTypes from "prop-types";
-import { userCodeFiles } from "../../contexts/UserCodeFilesContex";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import SelectMultipleCodeModal from '../modals/SelectMutipleCodeModal';
 
 const LoadServerFileButton = ({changeFileName},props) => {
 
-    const { setCodeFiles } = userCodeFiles()
-    const [openModal, setOpenModal] = useState(false);
-
-    const config = JSON.parse(
-        document.getElementById("exercise-config").textContent
-      );
-    
-    const SERVER_PORT = window.DJANGO_ENV.SERVER_PORT;
-    
-    useEffect(() => {
-      getCodes()
-    }, []);
-
-    const getCodes = async() => {
-        window.RoboticsReactComponents.MessageSystem.Loading.showLoading("Saving code...");
-        const serverBase = `${document.location.protocol}//${document.location.hostname}:${SERVER_PORT}`;
-    
-        let requestUrl = `${serverBase}/exercises/exercise/${config[0].exercise_id}/list_user_codes`;
-    
-        try {
-          const response = await fetch(requestUrl, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          });
-    
-          if(response.ok){
-            const data = await response.json()
-            setCodeFiles(data.codes);
-          }else
-            alert('Erro na resposta da API');
-          
-        } catch (error) {
-          console.log("ERRO: "+error)
-          alert('Erro no local');
-        }finally{window.RoboticsReactComponents.MessageSystem.Loading.hideLoading();}
-         
-    };
+  const [openModal, setOpenModal] = useState(false);
 
   return (
     <Box sx={{ display: "flex" }}>
--- react_frontend/src/components/layout_components/ExerciseControl.js
+++ react_frontend/src/components/layout_components/ExerciseControl.js
@@ -12,18 +12,56 @@ import Frequencies from "Components/visualizers/Frequencies";
 import PlayPauseButton from "Components/buttons/PlayPauseButton";
 import "../../styles/layout_components/ExerciseControl.css";
 import monitor from "../../images/monitoring2.png";
+import { userCodeFiles } from "../../contexts/UserCodeFilesContex";
 
 function ExerciseControl(props) {
   const [editorRendered, setEditorRendered] = React.useState(false);
   const [showFrequencies, setShowFrequencies] = React.useState(false);
   const [buttonActive, setButtonActive] = React.useState(false);
   const [fileName, setFileName] = React.useState("myCode");
+  const { setCodeFiles } = userCodeFiles()
+
+  const SERVER_PORT = window.DJANGO_ENV.SERVER_PORT;
+
+  const config = JSON.parse(
+    document.getElementById("exercise-config").textContent
+  );
+
+  //Find exercises's code files of user and put in
+  //codeFiles (context variable)
+  const getCodes = async() => {
+    const serverBase = `${document.location.protocol}//${document.location.hostname}:${SERVER_PORT}`;
+    let requestUrl = `${serverBase}/exercises/exercise/${config[0].exercise_id}/list_user_codes`;
+
+    try {
+      const response = await fetch(requestUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if(response.ok){
+        const data = await response.json()
+        setCodeFiles(data.codes);
+      }else
+        alert('Códigos dos exercicios não foram carregados: erro na resposta da API');
+      
+    } catch (error) {
+      console.log("ERRO: "+error)
+      alert('Códigos dos exercicios não foram carregados: erro local');
+    }finally{}
+  };
 
   React.useEffect(() => {
-    if (document.getElementById("code-container")) {
+    const getFilesCode = async () => {await getCodes();};
+
+    if (document.getElementById("code-container")) 
       setEditorRendered(true);
-    }
-  });
+    
+    getFilesCode()
+  },[]);
+
   const handleToggleFrequencies = () => {
     setButtonActive(!buttonActive);
     setShowFrequencies(!showFrequencies);
@@ -37,7 +75,6 @@ function ExerciseControl(props) {
     if (!invalidChars.test(name)) {
       setFileName(name);
     }
-  
   };
 
   return (
