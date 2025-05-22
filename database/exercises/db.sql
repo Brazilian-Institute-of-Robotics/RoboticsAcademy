8bc24f51899649b596fbe957b9edd06e82efd45a
barretto-dev <barretto.dev@gmail.com>
2025-05-22 15:59:54 -0300

    Add exercicse "empty_world" on db

--- database/exercises/db.sql
+++ database/exercises/db.sql
@@ -101,6 +101,7 @@ COPY public.exercises (id, exercise_id, name, description, tags, status, templat
 15	basic_computer_vision	Basic Computer Vision	Basic Computer Vision exercise using React and RAM	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/basic_computer_vision/python_template/
 16	follow_road	Follow Road	Follow Road exercise	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/follow_road/python_template/
 17	model_exercise	Model Exercise	exercicio modelo	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/model_exercise/python_template/
+18	empty_world	Empty World	mundo vazio	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/empty_world/python_template/
 \.
 -- 16	rescue_people_classic	Rescue People Old	Rescue People exercise With Gazebo Classic	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/rescue_people_classic/python_template/
 -- 17	teste_01	Teste01	teste exercise	{"tags": "ROS2"}	ACTIVE	RoboticsAcademy/exercises/static/exercises/teste_01/python_template/
@@ -142,6 +143,7 @@ COPY public.exercises_universes (id, exercise_id, universe_id) FROM stdin;
 30	16	32
 31	14	33
 32	17	33
+33	18	34
 \.
 -- 30  16  3
 
