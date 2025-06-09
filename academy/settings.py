954270d7e9f8699076a14f74a8c115007d0ccdfa
barretto-dev <barretto.dev@gmail.com>
2025-06-09 16:42:18 -0300

    Add SECRET_KEY on .env.example

--- academy/settings.py
+++ academy/settings.py
@@ -27,7 +27,7 @@ print("BASE_DIR", BASE_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--(47x6f9ah3wi=x-wuryrzl^&$kr+di_j#1a@-#%!(k+i6fbcd'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--(47x6f9ah3wi=x-wuryrzl^&$kr+di_j#1a@-#%!(k+i6fbcd')
 
 # GET PRODUCTION on .env and
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() in ('true', '1', 't')
