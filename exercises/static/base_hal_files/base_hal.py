e42714ebf3a4549aee60d90f728b03072a47ef27
barretto-dev <barretto.dev@gmail.com>
2025-08-18 16:39:09 -0300

    Correct base_hal.py
    In some exercises, the HAL.py generated from base_hal.py apresented errors, So the base file was modify to correct it
    
    the txt files were modified only to correct  indentation

--- exercises/static/base_hal_files/base_hal.py
+++ exercises/static/base_hal_files/base_hal.py
@@ -15,35 +15,37 @@ import time
 freq = 30.0
 
 print("HAL initializing", flush=True)
+
+def __auto_spin() -> None:
+    while rclpy.ok():
+        executor.spin_once(timeout_sec=0)
+        time.sleep(1/freq)
+
+# ROS2 init
 if not rclpy.ok():
     rclpy.init(args=sys.argv)
 
-    # CREATE BUMPER NODE
-    # CREATE CAMERA NODE
-    # CREATE LASER NODE
-    # CREATE MOTORS NODE
-    # CREATE NEURAL_NETWORK NODE
-    # CREATE NOISY_ODOMETRY NODE
-    # CREATE ODOMETRY NODE
-    # CREATE SIM_TIME NODE
+# CREATE BUMPER NODE
+# CREATE CAMERA NODE
+# CREATE LASER NODE
+# CREATE MOTORS NODE
+# CREATE NEURAL_NETWORK NODE
+# CREATE NOISY_ODOMETRY NODE
+# CREATE ODOMETRY NODE
+# CREATE SIM_TIME NODE
 
-    # Spin nodes so that subscription callbacks load topic data
-    executor = rclpy.executors.MultiThreadedExecutor()
-   
-    # ADD BUMPER NODE
-    # ADD CAMERA NODE
-    # ADD LASER NODE
-    # ADD NOISY_ODOMETRY NODE
-    # ADD ODOMETRY NODE
-    # ADD SIM_TIME NODE
+# Spin nodes so that subscription callbacks load topic data
+executor = rclpy.executors.MultiThreadedExecutor()
 
-    def __auto_spin() -> None:
-        while rclpy.ok():
-            executor.spin_once(timeout_sec=0)
-            time.sleep(1/freq)
-    executor_thread = threading.Thread(target=__auto_spin, daemon=True)
-    executor_thread.start()
+# ADD BUMPER NODE
+# ADD CAMERA NODE
+# ADD LASER NODE
+# ADD NOISY_ODOMETRY NODE
+# ADD ODOMETRY NODE
+# ADD SIM_TIME NODE
 
+executor_thread = threading.Thread(target=__auto_spin, daemon=True)
+executor_thread.start()
 
 # BUMPER NODE FUNCTIONS
 # CAMERA NODE FUNCTIONS
